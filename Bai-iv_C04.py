dac16dea44d30dbf32c0d0ebad155a45712cf137
BaoLong <baolongkg2020@gmail.com>
2026-04-21 11:38:31 +0700

    TH

---
+++ Bai-iv_C04.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+
+now = datetime.now()
+print(f"Thời gian hiện tại: {now.strftime('%H:%M:%S')}")
+
+new_time = now + timedelta(seconds=5)
+print(f"Thời gian sau khi thêm 5 giây: {new_time.strftime('%H:%M:%S')}")
